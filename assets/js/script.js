f9be6183da99900870f5afbaba91684d5d6d1426
btbautista31 <134978501+btbautista31@users.noreply.github.com>
2023-08-02 22:51:26 -0500

    Added 'Submit Score' function and search/input event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Function to start the quiz
+// Setting global variables
 var questionIndex = 0
 var questionEl = document.getElementById("question")
 var answersEl = document.getElementById("answer-choices")
@@ -10,36 +10,50 @@ var timeLeft = 90
 var interval
 var startScreen = document.getElementById("start")
 var gameScreen = document.getElementById("game")
+var scoreScreen = document.getElementById("scores")
+var correctCounter = 0
+var currentPlayer = document.getElementById("name-input")
+
 
 //Function to generate the questions and associate to correct answers
 var questions = [
     {
         question: "question1",
-        answers: ["answer1", "answer2", "answer3", "answer4"], answer: "answer2"
-    },
-    {
-        question: "question2",
         answers: ["answer1", "answer2", "answer3", "answer4"], answer: "answer1"
     },
     {
+        question: "question2",
+        answers: ["answer1", "answer2", "answer3", "answer4"], answer: "answer2"
+    },
+    {
         question: "question3",
-        answers: ["answer1", "answer2", "answer3", "answer4"], answer: "answer4"
+        answers: ["answer1", "answer2", "answer3", "answer4"], answer: "answer3"
     },
     {
         question: "question4",
-        answers: ["answer1", "answer2", "answer3", "answer4"], answer: "answer3"
+        answers: ["answer1", "answer2", "answer3", "answer4"], answer: "answer4"
     }
 
 ]
-function startTimer () {
+
+
+//Object for collating top 10 scores
+var highScores = [
+    {
+    playerName: [],
+    playerScore: [],
+    }
+
+]
+
+function startTimer() {
     interval = setInterval(() => {
         timerEl.textContent = timeLeft
         if (timeLeft < 1) {
             console.log("Game Over")
-            
         } else {
-            timeLeft --;
-        }    
+            timeLeft--;
+        }
     }, 1000);
 }
 
@@ -47,34 +61,24 @@ function startTimer () {
 function displayQuestions() {
     questionEl.innerText = ""
     answersEl.innerHTML = ""
-    // msgEl.innerText = ""
-    if (questionIndex<questions.length) {
+
+    if (questionIndex < questions.length) {
         questionEl.innerText = questions[questionIndex].question
         rightAnswer = questions[questionIndex].answer
-    let choices = questions[questionIndex].answers
-    for (let i = 0; i < choices.length; i++) {
-        const element = choices[i];
-        let answerBtn = document.createElement("button")
-        answerBtn.classList.add("answer-btn")
-        answerBtn.innerText = element
-        answersEl.append(answerBtn)
-        
-    }
+        let choices = questions[questionIndex].answers
+        for (let i = 0; i < choices.length; i++) {
+            const element = choices[i];
+            let answerBtn = document.createElement("button")
+            answerBtn.classList.add("answer-btn")
+            answerBtn.innerText = element
+            answersEl.append(answerBtn)
+        }
     } else {
         alert("Game Over")
     }
 }
 
-function startGame(event) {
-    event.preventDefault()
-    console.log("Game has started")
-    startScreen.style.display = "none"
-    gameScreen.style.display = "block"
-    startTimer();
-    displayQuestions();
-
-}
-function selectAnswer (event) {
+function selectAnswer(event) {
     event.preventDefault();
     if (event.target && event.target.matches(".answer-btn")) {
         console.log(event.target.innerText)
@@ -82,18 +86,57 @@ function selectAnswer (event) {
         if (userAnswer === rightAnswer) {
             console.log(msgEl)
             msgEl.innerText = "Correct Answer"
-            
+            correctCounter++;
         } else {
-            console.log(msgEl)    
+            console.log(msgEl)
             msgEl.innerText = "Incorrect"
             // remove 10 seconds from the timer
             timeLeft -= 10;
+            
         }
-        questionIndex ++;
+        questionIndex++;
         displayQuestions();
+        endGame();
     }
 }
-//
-//Function to collate high scores
+
+//Function: Start timer and display questions
+function startGame(event) {
+    event.preventDefault()
+    console.log("Game has started")
+    startScreen.style.display = "none"
+    gameScreen.style.display = "block"
+    startTimer();
+    displayQuestions();
+}
+
+//Function: End Game, Enter name, Score Display
+function endGame() {
+    if (questionIndex >= questions.length) {
+        console.log("Out of questions")
+        gameScreen.style.display = "none"
+        scoreScreen.style.display = "block"
+        document.getElementById("player-score").textContent = correctCounter;
+        console.log(correctCounter)
+
+    }
+
+}
+
+function submitScore (event) {
+    var playerName = document.getElementById("name-input").value
+    var playerScore = correctCounter
+    console.log("Player Name:" + playerName);
+    console.log("Player Score: " + playerScore);
+}
+
+
+//Event Listeners
 document.addEventListener("click", selectAnswer)
 document.getElementById("beginBtn").addEventListener("click", startGame)
+document.getElementById("submitBtn").addEventListener("click", submitScore)
+currentPlayer.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        submitScore();
+    }
+});
